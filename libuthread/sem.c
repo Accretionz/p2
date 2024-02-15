ed3d93b88d07360ba10afd121b6ed18a29666f52
Accretionz <jieliu415@gmail.com>
2024-02-14 16:35:01 -0800

    Co-authored-by: Aroshia <Aroshia@users.noreply.github.com>

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -8,20 +8,22 @@
 struct semaphore {
 	/* TODO Phase 3 */
 	size_t count; // Number of resources
-	queue_t waiting_threads;
+	queue_t waiting_threads; // Queue for threads waiting for resources
 };
 
 sem_t sem_create(size_t count)
 {
 	/* TODO Phase 3 */
+	// Allocation of space for semaphore
 	sem_t sem = malloc(sizeof(struct semaphore));
 	if (!sem) return NULL;
 
+	// Initialize sem count and waiting thread queue
 	sem->count = count;
 	sem->waiting_threads = queue_create();
 	if (!sem->waiting_threads)
 	{
-		free(sem);
+		free(sem); // Free sem if queue can't be created
 		return NULL;
 	}
 
@@ -31,11 +33,13 @@ sem_t sem_create(size_t count)
 int sem_destroy(sem_t sem)
 {
 	/* TODO Phase 3 */
+	// Check if semaphore is NULL or there is still waiting threads
 	if (!sem || queue_length(sem->waiting_threads) > 0)
 	{
 		return -1;
 	}
 
+	// Destroy waiting threads queue and free semaphore
 	queue_destroy(sem->waiting_threads);
 	free(sem);
 	return 0;
@@ -46,13 +50,16 @@ int sem_down(sem_t sem)
 	/* TODO Phase 3 */
 	if (!sem) return -1;
 
+	preempt_disable(); // Disable preempts to prevent race conditions
 	while(sem->count = 0)
-	{
+	{	
+		// Block thread if no resource available
 		uthread_block();
 		queue_enqueue(sem->waiting_threads, (void *)pthread_self());
 	}
 
 	sem->count--;
+	preempt_enable();
 
 }
 
@@ -61,15 +68,18 @@ int sem_up(sem_t sem)
 	/* TODO Phase 3 */
 	if (!sem) return -1;
 
-	sem->count++;
+	preempt_disable();
+	sem->count++; // Increase semaphore count
 
 	if (queue_length(sem->waiting_threads) > 0)
-	{
+	{	
+		// Unblocking first thread in queue
 		uthread_t tid;
 		queue_dequeue(sem->waiting_threads, (void **)&tid);
 		uthread_unblock(tid);
 	}
 
+	preempt_enable();
 	return 0;
 }
 
