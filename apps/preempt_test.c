8e413e769f78d831dfbee47c7537402a18e87364
Accretionz <156376194+Accretionz@users.noreply.github.com>
2024-02-16 23:09:16 -0800

    Create preempt_test.c

---
+++ apps/preempt_test.c
@@ -0,0 +1,35 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <uthread.h>
+
+#define TEST_ASSERT(assert)                \
+do {                                    \
+    printf("ASSERT: " #assert " ... ");    \
+    if (assert) {                        \
+        printf("PASS\n");                \
+    } else    {                            \
+        printf("FAIL\n");                \
+        exit(1);                        \
+    }                                    \
+} while(0)
+
+void thread2(void *arg){
+  (void)arg;
+  uthread_exit();
+}
+
+void thread1(void *arg){
+  (void)arg;
+
+  uthread_create(thread2, NULL);
+  while(1){}
+}
+
+
+int main(){
+  bool preempt = true;
+  int test = 1;
+  uthread_run(preempt, thread1, NULL);
+}
